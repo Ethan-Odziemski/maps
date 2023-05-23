757b271f15df415bbd647b513acf8b72d961db78
Ethan <ethan.odziemski@fujitsu.com>
2023-05-23 10:20:18 +0100

    1st commit

---
+++ maps/src/App.js
@@ -0,0 +1,26 @@
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./App.css";
+
+const App = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyD9AmQ0dAeA3gfJZxQxQI8dXXjDMQ8Bqd0",
+  });
+  const center = useMemo(() => ({ lat: 53.50400, lng: -2.19980 }), []);
+
+  return (
+    <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={17}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
